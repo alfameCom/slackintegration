603a285ffad177c82a9a2984816113ab932f694a
Hannu Kosonen <hannu.kosonen@alfame.com>
2019-03-23 18:06:19 +0200

    Added vwBITotallyUseless view

---
+++ SlackIntegration.Database/Views/vwBITotallyUseless.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[vwBITotallyUseless]
+	AS SELECT bn.*, ssr.Id as ReceiverID, ssr.Receiver
+  FROM [dbo].[vwBINonsense] bn
+  JOIN [dbo].[SlackSuccessReceiver] ssr on bn.id=ssr.SuccessId
