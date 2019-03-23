30bc0b5d3c752ae1e871b05df14124ee4e0e04e0
Hannu Kosonen <hannu.kosonen@alfame.com>
2019-03-23 11:20:27 +0200

    Changed id field to identity field

--- SlackIntegration.Data/SlackConfiguration.sql
+++ SlackIntegration.Data/SlackConfiguration.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[SlackConfiguration]
 (
-	[Id] BIGINT NOT NULL PRIMARY KEY, 
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [Key] VARCHAR(50) NOT NULL, 
     [Value] VARCHAR(MAX) NOT NULL
 )
