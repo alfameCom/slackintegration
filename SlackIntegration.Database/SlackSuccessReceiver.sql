fa513238d4499943a4394ff06ee0cbb9da19a5f6
Hannu Kosonen <hannu.kosonen@alfame.com>
2019-03-23 12:20:07 +0200

    Added identity field to slacksuccessreceiver

--- SlackIntegration.Database/SlackSuccessReceiver.sql
+++ SlackIntegration.Database/SlackSuccessReceiver.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[SlackSuccessReceiver]
 (
-	[Id] BIGINT NOT NULL PRIMARY KEY, 
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
 	[SuccessId] BIGINT NOT NULL,
     [Receiver] NVARCHAR(50) NOT NULL, 
     CONSTRAINT [FK_SlackSuccessReceiver_SlackSuccess] FOREIGN KEY ([SuccessId]) REFERENCES [SlackSuccess]([Id]) 
